220a2a211f9274075162205718bbdacd43a5c6bc
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2024-02-22 16:00:28 +0200

    redesign and button functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [lang, setLang] = useState("");
+  const [english, setEnglish] = useState("");
+
+  const translate = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setEnglish(lang)
+  };
+
+  const handleEnglishChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setLang(e.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl">Hello world</h1>
-    </main>
+    <>
+      <nav className="flex py-5 justify-center">
+        <h1 className="text-3xl font-bold">Translator</h1>
+      </nav>
+      <div className=" flex w-full justify-center items-center">
+        <div className="flex flex-row justify-center items-center gap-6 w-full mt-5">
+          <div>
+            <form action="" className="flex items-center space-x-5">
+              <textarea
+                name="english"
+                id="english"
+                cols={30}
+                rows={10}
+                className="border-2 border-black"
+                onChange={handleEnglishChange}
+              ></textarea>
+              <button type="submit" onClick={translate} className="bg-red-500 p-1 rounded-md ">
+                submit
+              </button>
+            </form>
+          </div>
+          <div>
+            <form action="">
+              <textarea 
+              name="lang" 
+              id="lang" 
+              cols={30} 
+              rows={10} 
+              className="border-2 border-black"
+              value={english}
+              readOnly
+              ></textarea>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
