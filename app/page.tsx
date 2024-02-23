41a068da228d70d2bdcec457ebce09f1f8ad04f9
icep0ps <tapsmuko@gmail.com>
2024-02-23 23:19:46 +0200

    added error handling

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,35 @@ import { useState } from 'react';
 export default function Home() {
   const [lang, setLang] = useState('');
   const [english, setEnglish] = useState('');
+  const [errors, setErrors] = useState('');
 
   async function query(data: { inputs: string }) {
-    const response = await fetch(
-      'https://api-inference.huggingface.co/models/icep0ps/marian-finetuned-kde4-en-to-rw',
-      {
-        headers: { Authorization: `Bearer ${process.env.MODEL_API_TOKEN}` },
-        method: 'POST',
-        body: JSON.stringify(data),
-      }
-    );
-    const result = await response.json();
-    return result;
+    try {
+      const response = await fetch(
+        'https://api-inference.huggingface.co/models/icep0ps/marian-finetuned-kde4-en-to-rw',
+        {
+          headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_MODEL_API_TOKEN}` },
+          method: 'POST',
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await response.json();
+      setErrors('');
+      return result;
+    } catch (error) {
+      throw new Error('error occured ' + error);
+    }
   }
 
   const translate = async (e: React.FormEvent) => {
     e.preventDefault();
-    query({ inputs: lang }).then((response) => {
-      setEnglish(response[0].generated_text);
-    });
+    query({ inputs: lang })
+      .then((response) => {
+        setEnglish(response[0].generated_text);
+      })
+      .catch((error) => {
+        setErrors('Error occured' + error);
+      });
   };
 
   const handleEnglishChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -34,6 +44,7 @@ export default function Home() {
     <>
       <nav className="flex py-5 justify-center">
         <h1 className="text-3xl font-bold">Translator</h1>
+        {errors != '' && <p>{errors} </p>}
       </nav>
       <div className=" flex w-full justify-center items-center">
         <div className="flex flex-row justify-center items-center gap-6 w-full mt-5">
